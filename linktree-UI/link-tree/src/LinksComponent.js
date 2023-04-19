4eb4ceea6cdabf4c199817b659418b855548d78c
saim <saim@koii.network>
2023-04-19 13:14:48 -0300

    working frontend

--- linktree-UI/link-tree/src/LinksComponent.js
+++ linktree-UI/link-tree/src/LinksComponent.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { truncateAddress } from "./helpers";
 import getNode from "./getNode";
+import axios from "axios";
 
 function LinksComponent({ links, publicKey }) {
   const [formattedLinks, setFormattedLinks] = useState([]);
